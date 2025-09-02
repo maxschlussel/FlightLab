161140018328c0f53cabe273591d9dd8222fbd06
Max Schlussel <maxschlussel@gmail.com>
2025-09-02 13:24:39 +0300

    fix: fixed plot 3d axes

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -25,6 +26,10 @@ plt.tight_layout()
 plt.show()
 
 
+phi   = df["phi"]
+theta = df["theta"]
+psi   = df["psi"]
+time = df["time"]
 
 
 import numpy as np
@@ -33,35 +38,35 @@ from matplotlib.animation import FuncAnimation
 from scipy.spatial.transform import Rotation as R
 
 
-# Convert Euler angles to rotation matrices (ZYX convention: psi, theta, phi)
-rotations = R.from_euler('xyz', np.column_stack([df.phi, df.theta, df.psi])).as_matrix()
+n = len(phi)
 
-# Axes to plot: unit vectors in body frame
-body_axes = np.eye(3)  # x, y, z
+# Body axes in body frame
+body_axes = np.eye(3)
 
-# Setup plot
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.set_xlim([-1,1])
-ax.set_ylim([1,-1])
-ax.set_zlim([1,-1])
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
+ax = fig.add_subplot(111, projection="3d")
+ax.set_xlim([-1, 1])
+ax.set_ylim([-1, 1])
+ax.set_zlim([-1, 1])
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z (down)")
 
-# Create quivers for axes: red=x, green=y, blue=z
-quivers = [ax.quiver(0,0,0,1,0,0,color='r'),
-           ax.quiver(0,0,0,0,1,0,color='g'),
-           ax.quiver(0,0,0,0,0,1,color='b')]
+lines = [ax.plot([0,0],[0,0],[0,0])[0] for _ in range(3)]
+colors = ["r","g","b"]
 
 def update(i):
-    R_i = rotations[i]
-    for j, q in enumerate(quivers):
-        vec = R_i @ body_axes[:,j]
-        # Remove old quiver and redraw new one
-        q.remove()
-        quivers[j] = ax.quiver(0,0,0,vec[0],vec[1],vec[2],color=['r','g','b'][j])
-    return quivers
+    rot = R.from_euler("xyz", [phi[i], theta[i], psi[i]], degrees=True)
+    rotated_axes = rot.apply(body_axes)
 
-ani = FuncAnimation(fig, update, frames=len(df.time), interval=10)
+    for j in range(3):
+        lines[j].set_data([0, rotated_axes[j,0]], [0, rotated_axes[j,1]])
+        lines[j].set_3d_properties([0, -rotated_axes[j,2]])  # flip Z
+
+        lines[j].set_color(colors[j])
+
+    return lines
+
+plot_every = 10  # plot every 5 timesteps
+ani = FuncAnimation(fig, update, frames=range(0, len(time), plot_every), interval=plot_every*10)
 plt.show()
