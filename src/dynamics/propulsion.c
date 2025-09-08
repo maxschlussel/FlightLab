f83164ea04411cb29daac5c3c0fa8aa4bce37344
Max Schlussel <maxschlussel@gmail.com>
2025-09-08 11:47:40 +0300

    bug: fixed moment calculation and logging

--- src/dynamics/propulsion.c
+++ src/dynamics/propulsion.c
@@ -15,14 +15,14 @@ void computePropulsionForces(StateVector* X, ControlVector* U, AircraftParams* a
     *F = vec3_add(FengineOne, FengineTwo);
 
     Vector3 MomentEngineOne_b = vec3_cross(acParams->r_engineOne2cg, FengineOne);
-    Vector3 MomentEngineTwo_b = vec3_cross(acParams->r_engineOne2cg, FengineTwo);
+    Vector3 MomentEngineTwo_b = vec3_cross(acParams->r_engineTwo2cg, FengineTwo);
 
     *M = vec3_add(MomentEngineOne_b, MomentEngineTwo_b);
 
     logger.data[LOG_FORCES_F_PROP_X] = F->x;
     logger.data[LOG_FORCES_F_PROP_Y] = F->y;
     logger.data[LOG_FORCES_F_PROP_Z] = F->z;
-    logger.data[LOG_MOMENTS_M_PROP_X] = F->x;
-    logger.data[LOG_MOMENTS_M_PROP_Y] = F->y;
-    logger.data[LOG_MOMENTS_M_PROP_Z] = F->z;
+    logger.data[LOG_MOMENTS_M_PROP_X] = M->x;
+    logger.data[LOG_MOMENTS_M_PROP_Y] = M->y;
+    logger.data[LOG_MOMENTS_M_PROP_Z] = M->z;
 }
