b8ee80ee292a01b7276bf86f60c32f043b89a390
Max Schlussel <maxschlussel@gmail.com>
2025-09-17 16:35:30 +0300

    style: updated init conditions

--- src/sim/scenarios.c
+++ src/sim/scenarios.c
@@ -7,14 +7,14 @@
 * @brief Load the initial conditions of the aircraft control vector.
 */
 ControlVector initControlVectorlBasic(void){
-    ControlVector U = {
+    ControlVector U_cmd = {
         .da = 0.0,
-        .de = 3.0,
+        .de = -5.0*deg2rad,
         .dr = 0.0,
         .dt[0] = 0.0,
         .dt[1] = 0.0
     };
-    return U;
+    return U_cmd;
 }
 
 /**
@@ -22,18 +22,18 @@ ControlVector initControlVectorlBasic(void){
 */
 StateVector initStateVectorBasicCruise(void){
     StateVector X = {
-        .u = 50.0,
+        .u = 80.0,
         .v = 0.0,
         .w = 0.0,
         .p = 0.0,
         .q = 0.0,
         .r = 0.0,
-        .phi    = 0.0,
-        .theta  = 0.0,
-        .psi    = 0.0,
+        .phi    = 0.1,
+        .theta  = 0.1,
+        .psi    = 0.1,
         .x = 0.0,
         .y = 0.0,
-        .z = 1000.0
+        .z = -1000.0
     };
     return X;
 }
